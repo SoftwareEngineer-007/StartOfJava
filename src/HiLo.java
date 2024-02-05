957763ab473b13dd130bf6b384ee5e3ddeaeba5c
Alex Selyutin <selutin.alexandr90@gmail.com>
2024-02-06 01:00:40 +0300

    add scanner

--- src/HiLo.java
+++ src/HiLo.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class HiLo {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         // Create a random number for the user to guess
         int theNumber = (int)(Math.random() * 100 + 1);
         System.out.println(theNumber);
+        int guess = 0;
     }
 }
