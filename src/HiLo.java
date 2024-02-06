7d2cfef8ec335b3eba590d3bfdc1bf9e35edc5b1
Alex Selyutin <selutin.alexandr90@gmail.com>
2024-02-07 01:39:22 +0300

    while

--- src/HiLo.java
+++ src/HiLo.java
@@ -5,9 +5,12 @@ public class HiLo {
         Scanner scan = new Scanner(System.in);
         // Create a random number for the user to guess
         int theNumber = (int)(Math.random() * 100 + 1);
-        System.out.println(theNumber);
+        // System.out.println(theNumber);
         int guess = 0;
-        System.out.println("Guess a number between 1 and 100: ");
-        guess = scan.nextInt();
+        while (guess != theNumber) {
+            System.out.println("Guess a number between 1 and 100: ");
+            guess = scan.nextInt();
+            System.out.println("You entered " + guess + ".");
+        }
     }
 }
