4d6d69b9e73fc344182cfc1fe8d15fb7a65a4726
Alex Selyutin <selutin.alexandr90@gmail.com>
2024-02-07 01:24:32 +0300

    request a number from the user

--- src/HiLo.java
+++ src/HiLo.java
@@ -7,5 +7,7 @@ public class HiLo {
         int theNumber = (int)(Math.random() * 100 + 1);
         System.out.println(theNumber);
         int guess = 0;
+        System.out.println("Guess a number between 1 and 100: ");
+        guess = scan.nextInt();
     }
 }
